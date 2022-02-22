9122489925c45f5f46eb2b0b37b4e47f9d8b58e7
APerezBanks <88187553+APerezBanks@users.noreply.github.com>
2022-02-22 10:19:29 +0000

    last commit

--- src/app.js
+++ src/app.js
@@ -2,30 +2,37 @@ const yargs = require("yargs");
 const Movie = require("./utils");
 
 const app = (yargsObj) => {
+    console.log(yargsObj)
+    console.log(yargsObj.title)
+    console.log(yargsObj.actor)
+
     if (yargsObj.add) {
         const movie = new Movie(yargsObj.title, yargsObj.actor);
         movie.add();
         console.log(movie.list());
+    
+    } else if (yargsObj.addNew) {
+
+        const movie1 = new Movie (yargsObj.title, yargsObj.actor ); 
+        const movie2 = new Movie (yargsObj.nothing, yargsObj.fish);    
+            movie1.add();
+            movie2.add();
+            console.log(movie1.list());
+          
+        
+
+        const allMovies = [movie1, movie2]
+        // console.log(yargsObj.title)
+        // console.log(yargsObj.actor)
 
 
 
-
-
-
-
-      // add a movie to array, cosole log at the end of runtime ///
-    //  } else if (yargsObj) {
-    //     const movie1 = new Movie (yargsObj.tittle, yargsObj.actor);
-    //     movie1.add();
-    //     console.log(movie1.list()); 
-
+        
     } else {
-
+        
             console.log('Incorrect command')
+
     }
     };
     
-
-
-
 app(yargs.argv);
